32a1c0fc78348ee1b6038ba6ca53137f873a28dc
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2017-11-04 18:15:56 -0300

    troca dos locais de arquivos

--- App-Multiplataforma/www/js/index.js
+++ App-Multiplataforma/www/js/index.js
@@ -1,46 +1,26 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var selBtnIniciar = document.querySelector(".btn-iniciar");
+var selGameTitle = document.querySelector(".game-title");
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+selBtnIniciar.addEventListener("click",function(){
+	selBtnIniciar.parentElement.removeChild(selBtnIniciar);
+	game();
+})
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function game(){
+	
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
+function onError() {
+    alert('onError!');
+}
 
-app.initialize();
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+
+selGameTitle.innerHTML = acceleration.x;
