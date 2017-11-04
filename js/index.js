db6b67e2bfb44b063cf168556a6164940b019f0c
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2017-11-04 18:15:00 -0300

    instalação do cordova

--- js/index.js
+++ js/index.js
@@ -9,3 +9,18 @@ selBtnIniciar.addEventListener("click",function(){
 function game(){
 	
 }
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+
+selGameTitle.innerHTML = acceleration.x;
